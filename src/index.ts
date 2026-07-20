c7c42660ed7500825e9742752864788f10ddbd4e
Steluro <282664784+Steluro@users.noreply.github.com>
2026-07-20 17:01:16 -0400

    ready for publishing if ever it is

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Collaborator from "./models/collaborator.model";
+import ElementType from "./models/elementType.model";
+import EndpointType from "./models/endpointType.model";
+import Language from "./models/language.model";
+import RelationType from "./models/relationType.model";
+
+export { Collaborator, ElementType, EndpointType, Language, RelationType };
